c19757729ff6ac3d867e2455787afcb6cb6d09ad
Tom Paz <tomp@jfrog.com>
2025-03-30 23:03:33 +0300

    Adding slack notifications

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -26,126 +26,126 @@ def call(String buildStatus = 'STARTED', String deploymentName = 'Unknown') {
 
   def attachments = [
     [
-      "color": color,
-      "blocks": [
+      color: color,
+      blocks: [
         [
-          "type": "header",
-          "text": {
-            "type": "plain_text",
-            "text": "K8S Deployment - ${deploymentName} Pipeline ${env.emoji}",
-            "emoji": true
-          }
+          type: "header",
+          text: [
+            type: "plain_text",
+            text: "K8S Deployment - ${deploymentName} Pipeline ${env.emoji}",
+            emoji: true
+          ]
         ],
         [
-          "type": "section",
-          "fields": [
+          type: "section",
+          fields: [
             [
-              "type": "mrkdwn",
-              "text": "*Job Name:*\n${env.JOB_NAME}"
+              type: "mrkdwn",
+              text: "*Job Name:*\n${env.JOB_NAME}"
             ],
             [
-              "type": "mrkdwn",
-              "text": "*Build Number:*\n${env.BUILD_NUMBER}"
+              type: "mrkdwn",
+              text: "*Build Number:*\n${env.BUILD_NUMBER}"
             ]
           ],
-          "accessory": {
-            "type": "image",
-            "image_url": "https://raw.githubusercontent.com/sidd-harth/devsecops-k8s-demo/main/slack-emojis/jenkins.png",
-            "alt_text": "Slack Icon"
-          }
+          accessory: [
+            type: "image",
+            image_url: "https://raw.githubusercontent.com/sidd-harth/devsecops-k8s-demo/main/slack-emojis/jenkins.png",
+            alt_text: "Slack Icon"
+          ]
         ],
         [
-          "type": "section",
-          "text": {
-            "type": "mrkdwn",
-            "text": "*Failed Stage Name:* `${env.failedStage}`"
-          },
-          "accessory": {
-            "type": "button",
-            "text": {
-              "type": "plain_text",
-              "text": "Jenkins Build URL",
-              "emoji": true
-            },
-            "url": "${env.BUILD_URL}",
-            "action_id": "button-action"
-          }
+          type: "section",
+          text: [
+            type: "mrkdwn",
+            text: "*Failed Stage Name:* `${env.failedStage}`"
+          ],
+          accessory: [
+            type: "button",
+            text: [
+              type: "plain_text",
+              text: "Jenkins Build URL",
+              emoji: true
+            ],
+            url: "${env.BUILD_URL}",
+            action_id: "button-action"
+          ]
         ],
         [
-          "type": "divider"
+          type: "divider"
         ],
         [
-          "type": "section",
-          "fields": [
+          type: "section",
+          fields: [
             [
-              "type": "mrkdwn",
-              "text": "*Kubernetes Deployment Name:*\n${deploymentName}"
+              type: "mrkdwn",
+              text: "*Kubernetes Deployment Name:*\n${deploymentName}"
             ],
             [
-              "type": "mrkdwn",
-              "text": "*Node Port:*\n32564"
+              type: "mrkdwn",
+              text: "*Node Port:*\n32564"
             ]
           ],
-          "accessory": {
-            "type": "image",
-            "image_url": "https://raw.githubusercontent.com/sidd-harth/devsecops-k8s-demo/main/slack-emojis/k8s.png",
-            "alt_text": "Kubernetes Icon"
-          }
+          accessory: [
+            type: "image",
+            image_url: "https://raw.githubusercontent.com/sidd-harth/devsecops-k8s-demo/main/slack-emojis/k8s.png",
+            alt_text: "Kubernetes Icon"
+          ]
         ],
         [
-          "type": "section",
-          "text": {
-            "type": "mrkdwn",
-            "text": "*Kubernetes Node:* `controlplane`"
-          },
-          "accessory": {
-            "type": "button",
-            "text": {
-              "type": "plain_text",
-              "text": "Application URL",
-              "emoji": true
-            },
-            "url": "${applicationURL}:32564",
-            "action_id": "button-action"
-          }
+          type: "section",
+          text: [
+            type: "mrkdwn",
+            text: "*Kubernetes Node:* `controlplane`"
+          ],
+          accessory: [
+            type: "button",
+            text: [
+              type: "plain_text",
+              text: "Application URL",
+              emoji: true
+            ],
+            url: "${applicationURL}:32564",
+            action_id: "button-action"
+          ]
         ],
         [
-          "type": "divider"
+          type: "divider"
         ],
         [
-          "type": "section",
-          "fields": [
+          type: "section",
+          fields: [
             [
-              "type": "mrkdwn",
-              "text": "*Git Commit:*\n${env.GIT_COMMIT}"
+              type: "mrkdwn",
+              text: "*Git Commit:*\n${env.GIT_COMMIT}"
             ],
             [
-              "type": "mrkdwn",
-              "text": "*GIT Previous Success Commit:*\n${env.GIT_PREVIOUS_SUCCESSFUL_COMMIT}"
+              type: "mrkdwn",
+              text: "*GIT Previous Success Commit:*\n${env.GIT_PREVIOUS_SUCCESSFUL_COMMIT}"
             ]
           ],
-          "accessory": {
-            "type": "image",
-            "image_url": "https://raw.githubusercontent.com/sidd-harth/devsecops-k8s-demo/main/slack-emojis/github.png",
-            "alt_text": "Github Icon"
-          }
+          accessory: [
+            type: "image",
+            image_url: "https://raw.githubusercontent.com/sidd-harth/devsecops-k8s-demo/main/slack-emojis/github.png",
+            alt_text: "Github Icon"
+          ]
         ],
         [
-          "type": "section",
-          "text": {
-            "type": "mrkdwn",
-            "text": "*Git Branch:* `${env.GIT_BRANCH}`"
-          },
-          "accessory": {
-            "type": "button",
-            "text": {
-              "type": "plain_text",
-              "text": "Github Repo URL",
-              "emoji": true
-            },
-            "url": "${env.GIT_URL}",
-            "action_id": "button-action"
-          }
+          type: "section",
+          text: [
+            type: "mrkdwn",
+            text: "*Git Branch:* `${env.GIT_BRANCH}`"
+          ],
+          accessory: [
+            type: "button",
+            text: [
+              type: "plain_text",
+              text: "Github Repo URL",
+              emoji: true
+            ],
+            url: "${env.GIT_URL}",
+            action_id: "button-action"
+          ]
         ]
       ]
     ]
